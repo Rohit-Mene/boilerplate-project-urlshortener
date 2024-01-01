55b39764b28ec84d1f8d347d1d92593ce2f785b5
Rohit-Mene <52379271+Rohit-Mene@users.noreply.github.com>
2024-01-01 02:36:55 +0000

    ShortURL creation

--- index.js
+++ index.js
@@ -2,6 +2,14 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const { MongoClient } = require('mongodb');
+const urlparser = require('url');
+const dns = require('dns');
+const client = new MongoClient(process.env.DB_URL);
+const db = client.db("urlshortner");
+const urls = db.collection("urls");
+
+
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
@@ -10,13 +18,30 @@ app.use(cors());
 
 app.use('/public', express.static(`${process.cwd()}/public`));
 
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
 app.get('/', function(req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
-// Your first API endpoint
-app.get('/api/hello', function(req, res) {
-  res.json({ greeting: 'hello API' });
+//ShortURL Creation
+app.post('/api/shorturl', function(req, res) {
+  console.log(req.body);
+  const dnslookup = dns.lookup(urlparser.parse(req.body.url).hostname,async(err,address)=>{
+    if(!address){
+      res.json({error: "invalid url"});
+    }else{
+      const urlCnt = await urls.countDocuments({});
+      const urlDoc = {
+        url : req.body.url,
+        shorturl : urlCnt
+      }
+      const result = await urls.insertOne(urlDoc);
+      res.json({original_url : req.body.url, shor_url:urlCnt});
+      console.log(result);
+    }
+  })
 });
 
 app.listen(port, function() {
